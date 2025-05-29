27d7d4311b8b564f22108705c30a86755e75a906
Cuong251 <114635515+Cuong251@users.noreply.github.com>
2025-05-29 17:02:05 +0700

    Added model training

--- main.py
+++ main.py
@@ -1,36 +1,64 @@
+import sys
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer, ModelTrainerConfig
+
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
-from networksecurity.entity.config_entity import TrainingPipelineConfig
 
-import sys
+from networksecurity.entity.config_entity import (
+    DataIngestionConfig, 
+    DataValidationConfig, 
+    DataTransformationConfig, 
+    TrainingPipelineConfig
+)
 
-if __name__ == '__main__':
+
+def run_pipeline():
     try:
-        training_pipleline_config=TrainingPipelineConfig()
-        data_ingestion_config=DataIngestionConfig(training_pipleline_config)
-        data_ingestion=DataIngestion(data_ingestion_config)
-        logging.info("Initiate the data ingestion")
-        data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
-        logging.info("Data Initiation Completed")
+        # Configurations
+        training_pipeline_config = TrainingPipelineConfig()
+
+        # Data Ingestion
+        data_ingestion_config = DataIngestionConfig(training_pipeline_config)
+        data_ingestion = DataIngestion(data_ingestion_config)
+        logging.info("Initiating data ingestion")
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Data ingestion completed")
         print(data_ingestion_artifact)
-        data_validation_config = DataValidationConfig(training_pipleline_config)
-        data_validation=DataValidation(data_ingestion_artifact, data_validation_config)
-        logging.info("Initiate the data validation")
-        data_validation.initiate_data_validation()
-        print(data_ingestion_artifact)
-        data_validation_artifact=data_validation.initiate_data_validation()
-        logging.info("Data Validation Completed")
+
+        # Data Validation
+        data_validation_config = DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(data_ingestion_artifact, data_validation_config)
+        logging.info("Initiating data validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data validation completed")
         print(data_validation_artifact)
-        data_transformation_config = DataTransformationConfig(training_pipleline_config)
-        logging.info("Data Transformation started")
-        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+
+        # Data Transformation
+        data_transformation_config = DataTransformationConfig(training_pipeline_config)
+        logging.info("Data transformation started")
+        data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
         data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("Data transformation completed")
         print(data_transformation_artifact)
-        logging.info("Data Transformation completed")
+
+        # Model Training
+        logging.info("Model training started")
+        model_trainer_config = ModelTrainerConfig(training_pipeline_config)
+        model_trainer = ModelTrainer(
+            model_trainer_config=model_trainer_config,
+            data_transformation_artifact=data_transformation_artifact
+        )
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        logging.info("Model training completed")
+        print(model_trainer_artifact)
 
     except Exception as e:
-        raise NetworkSecurityException(e,sys)
+        logging.error("Pipeline failed", exc_info=True)
+        raise NetworkSecurityException(e, sys)
+
+
+if __name__ == '__main__':
+    run_pipeline()
--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,21 +1,28 @@
-import sys 
+import sys
+import traceback
 from networksecurity.logging import logger
-class NetworkSecurityException(Exception):
-    def __init__(self, error_message, error_details:sys):
-        self.error_message=error_message
-        _,_,exc_tb = error_details.exc_info()
 
-        self.lineno=exc_tb.tb_lineno
-        self.file_name=exc_tb.tb_frame.f_code.co_filename
+class NetworkSecurityException(Exception):
+    def __init__(self, error_message, error_details: sys):
+        self.error_message = error_message
+
+        _, _, exc_tb = error_details.exc_info()
+        self.lineno = exc_tb.tb_lineno
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
+
+        super().__init__(self.__str__())
 
     def __str__(self):
-        return "Error eccured in python script name [{0}] line number [{1}] error message".format(
-        self.file_name, self.lineno, str(self.error_message))
+        return (
+            f"Error occurred in python script [{self.file_name}] "
+            f"at line number [{self.lineno}]: {self.error_message}"
+        )
 
+# Test block
 if __name__ == '__main__':
     try:
         logger.logging.info("Enter the try block")
-        a=1/0
+        a = 1 / 0
         print("This will not be printed", a)
     except Exception as e:
-        raise NetworkSecurityException(e,sys)
+        raise NetworkSecurityException(e, sys)
